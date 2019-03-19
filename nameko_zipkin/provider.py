9a1c93b204602eb668f761193c87765b5983098f
Zbigniew Siciarz <zbigniew@siciarz.net>
2019-03-19 15:17:36 +0100

    Initialize trace if there's no header in context.

--- nameko_zipkin/provider.py
+++ nameko_zipkin/provider.py
@@ -24,23 +24,21 @@ class Zipkin(DependencyProvider):
 
     def get_dependency(self, worker_ctx):
         zipkin_attrs = _read_zipkin_attrs(worker_ctx)
-
-        # if there are no zipking attrs in context data, request isn't traced
-        # TODO: support trace initialization in such condition
-        if not zipkin_attrs:
-            logger.info('no zipkin attrs in context, not tracing')
-            return None
+        logger.debug('get_dependency zipkin attrs: {}'.format(zipkin_attrs))
         span = zipkin.zipkin_server_span(worker_ctx.service_name,
                                          worker_ctx.entrypoint.method_name,
                                          zipkin_attrs=zipkin_attrs,
-                                         transport_handler=self.transport.handle)
+                                         transport_handler=self.transport.handle,
+                                         sample_rate=100.0)
+        logger.debug('tracing {}.{}'.format(worker_ctx.service_name, worker_ctx.entrypoint.method_name))
         self.spans[worker_ctx.call_id] = span
         return span
 
     def worker_setup(self, worker_ctx):
         span = self.spans.get(worker_ctx.call_id)
         if span:
-            worker_ctx.data[PARENT_SPAN_ID_HEADER] = span.zipkin_attrs.span_id
+            logger.debug('starting span for {}.{}'.format(worker_ctx.service_name, worker_ctx.entrypoint.method_name))
+            worker_ctx.data[PARENT_SPAN_ID_HEADER] = span.zipkin_attrs_override.span_id
             start_span(span)
 
     def worker_teardown(self, worker_ctx):
@@ -48,13 +46,17 @@ class Zipkin(DependencyProvider):
         if span:
             stop_span(span)
             del self.spans[worker_ctx.call_id]
+            logger.debug('stopped span for {}.{}'.format(worker_ctx.service_name, worker_ctx.entrypoint.method_name))
 
 
 def _read_zipkin_attrs(worker_ctx):
     if TRACE_ID_HEADER not in worker_ctx.data:
-        return None
-    return zipkin.ZipkinAttrs(trace_id=worker_ctx.data[TRACE_ID_HEADER],
+        trace_id = generate_random_64bit_string()
+        logger.info('No {} header in context, created trace ID: {}'.format(TRACE_ID_HEADER, trace_id))
+    else:
+        trace_id = worker_ctx.data[TRACE_ID_HEADER]
+    return zipkin.ZipkinAttrs(trace_id=trace_id,
                               span_id=generate_random_64bit_string(),
-                              parent_span_id=worker_ctx.data[PARENT_SPAN_ID_HEADER],
-                              flags=worker_ctx.data[FLAGS_HEADER],
-                              is_sampled=worker_ctx.data[SAMPLED_HEADER] == '1')
+                              parent_span_id=worker_ctx.data.get(PARENT_SPAN_ID_HEADER),
+                              flags=worker_ctx.data.get(FLAGS_HEADER),
+                              is_sampled=worker_ctx.data.get(SAMPLED_HEADER) == '1')
