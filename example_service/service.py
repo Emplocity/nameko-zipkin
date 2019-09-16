a898198cf96059dcb8669f76bd2d1ff6220a2734
Zbigniew Siciarz <zbigniew@siciarz.net>
2019-09-16 14:47:06 +0200

    Format example_service with black.

--- example_service/service.py
+++ example_service/service.py
@@ -39,18 +39,17 @@ logger = logging.getLogger(__name__)
 
 
 class ExampleService:
-    name = 'example_service'
+    name = "example_service"
     zipkin = Zipkin()
-    example_service = RpcProxy('example_service')
+    example_service = RpcProxy("example_service")
 
     @rpc
     def traced_method(self):
-        return 'Find me in Zipkin!'
+        return "Find me in Zipkin!"
 
-    @http('GET', '/')
+    @http("GET", "/")
     def traced_handler(self, request):
-        self.zipkin.update_binary_annotations({
-            'browser': request.headers.get('User-Agent'),
-            'url': request.url,
-        })
+        self.zipkin.update_binary_annotations(
+            {"browser": request.headers.get("User-Agent"), "url": request.url}
+        )
         return self.example_service.traced_method()
